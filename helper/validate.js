9d2dfa577e55c475f3fcb3d59a2e41c9f533b717
Guillermo San Martín <guille.seba0802@gmail.com>
2023-03-09 11:25:12 -0300

    validate article

---
+++ helper/validate.js
@@ -0,0 +1,16 @@
+const validator = require("validator");
+
+const validateArticle = (params) => {
+  let validate_title =
+    !validator.isEmpty(params.title) &&
+    validator.isLength(params.title, { min: 1, max: 32 });
+  let validate_content = !validator.isEmpty(params.content);
+
+  if (!validate_content || !validate_title) {
+    throw new Error("Información no valida.");
+  }
+};
+
+module.exports = {
+  validateArticle,
+};
