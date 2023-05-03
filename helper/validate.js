e9a8986c6db358bdb0eed185a91a69f29a25f723
Guillermo San Martín <guille.seba0802@gmail.com>
2023-05-03 17:21:44 -0300

    validate article change

--- helper/validate.js
+++ helper/validate.js
@@ -3,7 +3,7 @@ const validator = require("validator");
 const validateArticle = (params) => {
   let validate_title =
     !validator.isEmpty(params.title) &&
-    validator.isLength(params.title, { min: 1, max: 32 });
+    validator.isLength(params.title, { min: 1, max: 480 });
   let validate_content = !validator.isEmpty(params.content);
 
   if (!validate_content || !validate_title) {
